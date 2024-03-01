8eedb47dd9ab48321bd8394de9dd591d79df5a79
Tom Laakso <tom.laakso@taiste.com>
2024-03-01 10:45:20 +0200

    Fix story sources

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,8 @@
-import '../src/index.css';
+import '../lib/index.css';
 
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
